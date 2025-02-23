0881015301db657e5aa4b29d72d0bfe45d7e309a
Ilaria  Caporali <ic@MBP-di-Ilaria.homenet.telecomitalia.it>
2025-02-23 10:49:24 +0100

    changed tests

--- tests/test_gwbird.py
+++ tests/test_gwbird.py
@@ -1,11 +1,9 @@
 import pytest
 import numpy as np
-
-def test_gwbird():
-    import gwbird
+import gwbird
+from gwbird.overlap import Response
 
 def test_Response():
-    from gwbird.overlap import Response
     f = np.logspace(0, 3, 100)
     R_ET2L45 = Response.overlap('ET L1', 'ET L2', f, 0, 't', shift_angle=np.pi/4)
     
